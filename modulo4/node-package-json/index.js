d341e031b7f683761461982d9e391d8174f3beb6
LopesEduarda <eduardaloopes2011@hotmail.com>
2022-05-24 13:08:02 -0300

    exercício 1

---
+++ modulo4/node-package-json/index.js
@@ -0,0 +1,26 @@
+// Respostas dos exercícios de hoje.
+
+//EXERCÍCIO 1:
+
+// a) Etapa 1: Salve um arquivo como index.js e com o código var arguments = process.argv ; console.log(arguments) ;
+// Etapa 2: execute o arquivo index.js usando o comando abaixo: node index.js.
+
+// b)
+const myName = 'Maria Eduarda'
+const myAge = Number("22")
+
+console.log(`Olá, ${myName}! Você tem ${myAge} anos.`)
+//consegui imprimir no console com o seguinte comando: npm run start!
+
+// c)
+const ageInSevenYears = Number(myAge + 7)
+console.log(`Olá, ${myName}! Você tem ${myAge} anos. Em sete anos você terá ${ageInSevenYears}.`)
+
+
+
+
+
+
+
+
+
---
+++ modulo4/node-package-json2/index.js
@@ -0,0 +1,13 @@
+//EXERCÍCIO 2:
+
+const soma = Number(process.argv[2])
+console.log(soma + soma)
+//aqui o resultado no console será 30, pois rodei o comando: npm run start 30 30.
+
+const subtraindo = Number(process.argv[2]);
+console.log(subtraindo - 10)
+//aqui o resultado no console será 20, pois eu rodei o comando: npm run start 30 30.
+
+const multiplicando = Number(process.argv[2]);
+console.log(multiplicando * multiplicando)
+//o resultado no console dessa operação foi 900, pois rodei o comando npm run start 30 30.
