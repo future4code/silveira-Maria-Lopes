548a050b4a04c6fcd77f579509b4088380f9c5b3
LopesEduarda <eduardaloopes2011@hotmail.com>
2022-07-20 19:58:51 -0300

    atualizações no menu

--- modulo6/Ambulnz/frontend/pizzaria/src/components/pizzaCard.js
+++ modulo6/Ambulnz/frontend/pizzaria/src/components/pizzaCard.js
@@ -4,9 +4,9 @@ import styled from "styled-components";
 import { useNavigate } from 'react-router-dom';
 
 export const DivCards = styled.div`
-  box-shadow: rgb(85, 91, 255) 0px 0px 0px 3px, rgb(255, 217, 19) 0px 0px 0px 9px, rgb(255, 156, 85) 0px 0px 0px 12px, rgb(255, 85, 85) 0px 0px 0px 15px;
+  box-shadow: rgb(0, 0, 0, 1) 0px 0px 0px 5px, rgb(255, 217, 19) 0px 0px 0px 4px, rgb(255, 156, 85) 0px 0px 0px 6px, rgb(255, 85, 85) 0px 0px 0px 8px;
   border-radius: 5px;
-  min-height:50vh;
+  min-height: 20vh;
   width: 20vw;
 `
 export const P = styled.div`
@@ -18,15 +18,26 @@ export const H3 = styled.h3`
   justify-content: center;
   padding: 5px;
 `
+export const Button = styled.button`
+height: 30px;
+display: flex;
+justify-content: center;
+align-items: center;
+align-self: center;
+
+`
 
 export default function cardPizzas(props) {
 
-    return (
-        <DivCards>
-            <H3>Pizzas Cards</H3>
-            <P><strong>Name:</strong> {props.name}</P>
-            <P><strong>Price:</strong> {props.price}</P>
-            <button>Realizar o pedido</button>
-        </DivCards>
-    )
+  return (
+
+    <DivCards>
+      <H3>Pizzas Cards</H3>
+      <P><strong>Name:</strong> {props.name}</P>
+      <P><strong>Price:</strong> {props.price}</P>
+      <img src={props.photo} alt={props.name} />
+      <Button>Realizar o pedido</Button>
+    </DivCards>
+
+  )
 }
--- modulo6/Ambulnz/frontend/pizzaria/src/pages/MenuPage.js
+++ modulo6/Ambulnz/frontend/pizzaria/src/pages/MenuPage.js
@@ -3,11 +3,40 @@ import axios from 'axios';
 import styled from 'styled-components';
 import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../services/url";
-import { DivCards, H3 } from '../components/pizzaCard'
+import { DivCards, H3, Button } from '../components/pizzaCard'
 
+export const DivMain = styled.div`
+display: flex;
+justify-content: space-evenly;
+align-items: center;
+align-self: center;
+background-color: gray;
+height: 100vh;
+flex-direction: row;
+flex-wrap: wrap;
+padding-top: 30px;  
+`
+
+const IMG = styled.img`
+width: 273px;
+height: 150px;
+
+`
+const H3Button = styled.h2`
+display: flex;
+justify-content: center;
+align-items: center;
+align-self: center;
+cursor: pointer;
+`
+const H3Price = styled.h3`
+font-size: 20px;
+text-align: center;
+`
 
 export default function MenuPage() {
     const [pizzas, setPizzas] = useState([]);
+    const [order, setOrder] = useState([]);
 
     // const navigate = useNavigate();
     const token = window.localStorage.getItem("AuthToken");
@@ -30,6 +59,20 @@ export default function MenuPage() {
             })
     }
 
+    const makeOrder = () => {
+        axios
+            .post(`${BASE_URL}order`, headers)
+            .then((res) => {
+                console.log(res.data)
+                // setOrder(res.data)
+                alert("Pedido realizado com sucesso!")
+            })
+            .catch((err) => {
+                console.log(err)
+                alert("Não foi possível realizar o seu pedido!")
+            })
+    }
+
     useEffect(() => {
         getAllPizzas()
     }, []);
@@ -38,20 +81,18 @@ export default function MenuPage() {
         return (
             <DivCards key={index}>
                 <H3>{pizza.name}</H3>
-                <h1>{pizza.price}</h1>
-
-                {/* <Imagem src={''} alt={pizza.name} /> */}
-                <button><h1>Realizar pedido</h1></button>
+                <IMG src={pizza.photo} alt={pizza.name} />
+                <H3Price>Preço: {pizza.price},00</H3Price>
+                <Button onClick={makeOrder}><H3Button>Realizar pedido</H3Button></Button>
             </DivCards>
         )
     })
 
- 
+
 
     return (
-        <div>
+        <DivMain>
             {mapPizzas}
-            qualquer coisinha
-        </div>
+        </DivMain>
     )
 }
